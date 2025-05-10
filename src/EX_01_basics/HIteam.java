eabdc6bab60fda2b354cf93655ed1183c20f2bc2
kaarthick <kaarthickmurali@gmail.com>
2025-05-10 17:23:11 +0530

    Third Program

--- src/EX_01_basics/HIteam.java
+++ src/EX_01_basics/HIteam.java
@@ -3,6 +3,9 @@ package EX_01_basics;
 public class HIteam {
     public static void main(String[] args) {
 
-        System.out.println("HI TEAM");
+        System.out.println(10/2);
+        System.out.print("100");
+
+
     }
 }
---
+++ src/EX_01_basics/Multiple.java
@@ -0,0 +1,11 @@
+package EX_01_basics;
+
+public class Multiple {
+    public static void main (String[] args) {
+
+            System.out.println("multiple string");
+
+        }
+
+    }
+
